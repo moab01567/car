f998cba805df2cb3bd418c9aef44dbb26005e4cf
Mohemmed <moh080203@gmail.com>
2024-10-21 17:22:22 +0200

    removed test

--- CarsBackend/src/test/java/site/mohememd/CarsBackend/CarsBackendApplicationTests.java
+++ CarsBackend/src/test/java/site/mohememd/CarsBackend/CarsBackendApplicationTests.java
@@ -1,19 +1,11 @@
 package site.mohememd.CarsBackend;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import site.mohememd.CarsBackend.car.CarRestController;
-import site.mohememd.CarsBackend.car.filterSelectionsHandler.CarSelections;
-
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CarsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
