86883f145aac47507aed84ffd2d3fd58494398d7
moab015 <moab015@student.kristiania.no>
2025-01-14 23:42:08 +0100

    added a env file

---
+++ CarFrontend/env.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+export const REST_API_URL = import.meta.env.VITE_REST_API_URL;
--- CarFrontend/src/features/carFilterFeature/service/CarFilterService.ts
+++ CarFrontend/src/features/carFilterFeature/service/CarFilterService.ts
@@ -1,9 +1,9 @@
 import {SelectFilterCode} from "../Enum";
 import {CarDTO, SelectFilterDTO} from "../DTO/AllCarFilterDTOs";
-
+import {REST_API_URL} from "../../../../env";
 
 export async function APIGetAvailableCars() : Promise<CarDTO[]>{
-    const response = await fetch("http://localhost:8080/car/filter/available/cars")
+    const response = await fetch(REST_API_URL+"/car/filter/available/cars")
     if( response.ok){
         return await response.json()
     }else throw Error("something went wrong")
